54d06587f8b3fce9ddc0188c9b839ec39bf5540f
Muhammad Affan <m.affuquraishi@gmail.com>
2023-11-18 21:27:57 +0500

    this is the canvas file

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactFlow, { Controls } from 'react-flow-renderer';
 
 const Canvas = () => {
-  // Define nodes and edges for the React Flow canvas
+  
   const elements = [
-    // Define your nodes and edges here
-    // Example:
+
     { id: '1', type: 'input', data: { label: 'Node 1' }, position: { x: 0, y: 0 } },
     { id: '2', data: { label: 'Node 2' }, position: { x: 150, y: 0 } },
     { id: 'e1-2', source: '1', target: '2', animated: true },
